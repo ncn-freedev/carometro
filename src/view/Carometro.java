6f6d768fbdf044df3c9290a5e6c0bdd2eb4e4309
Nicolas <ncn.freedev@gmail.com>
2023-08-22 23:53:56 -0300

    Starting classes and configuring environment

---
+++ src/view/Carometro.java
@@ -0,0 +1,39 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+
+public class Carometro extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Carometro frame = new Carometro();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Carometro() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Carometro.class.getResource("/img/preferencessystemnetwork_104157.png")));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+	}
+
+}
